cea94a559964e8666199cf7f9c00f21b99bf6df1
root <root@static.113.99.4.46.clients.your-server.de>
2011-05-29 10:25:24 +0200

    inital

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shufazuopin::Application.config.secret_token = '48ac49e7397e93e12e79c61521fed0ababd2ce6dbd099ea78bb1a345f9afe811870968e517238dfda29299b86fa2082b4dfc4269557b7fa6291951d0cd8ed685'
