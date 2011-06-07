80d5e76ea1a7e082ca1c93ffe175e833add514db
Stefan Wienert <stwienert@gmail.com>
2011-06-07 22:54:03 +0200

    import cedict task and data

---
+++ lib/tasks/cedict.rb
@@ -0,0 +1,49 @@
+
+require "iconv"
+require "unicode_utils"
+
+def get_tone(pinyin)
+  stuff = pinyin.gsub(/[a-zA-Z]/, "")
+  if stuff.empty?
+    return 0
+  end
+
+  tone = case UnicodeUtils.char_name stuff
+         when /GRAVE/ then 4
+         when /ACUTE/ then 2
+         when /MACRON/ then 1
+         when /CARON/ then 3
+         else 0
+         end
+end
+
+doc = File.read("#{Rails.root}/import/cedict_1_0_ts_utf-8_mdbg.txt")
+lines = doc.split("\n")
+lines = lines.reject{|i| i[0] == "#"}
+
+tuples = lines.map{|line|
+  line.strip.match(/^(.*) (.*) \[(.*)\] \/(.*)\/$/)[1..5] rescue puts line
+}
+
+tuples.each do |tra,hanzi,pinyin,translation|
+  hanzi = hanzi.strip
+  pinyin = pinyin.downcase.strip
+  next if hanzi.length < 2
+
+  word = Word.find_or_initialize_by_hanzi(hanzi)
+  next unless word.new_record?
+
+  word.translation = translation.gsub("/","\n")
+  word.tone = get_tone pinyin
+  word.norm_pinyin = pinyin
+
+  word.pinyin = pinyin.split(" ").map{|char|
+    Character.find_by_norm_pinyin(char).pinyin rescue char
+  }.join(" ")
+
+  if translation.include? "CL:"
+    word.zew = translation.match(/CL:([^\[\]]+)/)[1][-1]
+  end
+  word.save
+end
+
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -7,4 +7,9 @@ namespace "import" do
     load "lib/tasks/xiaoma.rb"
   end
 
+
+  desc "cedict import"
+  task :cedict => :environment do
+    load "lib/tasks/cedict.rb"
+  end
 end
